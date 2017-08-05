cc08853f52c34e3b9914950774b30f22da239d83
sumshare <sumshare@126.com>
2017-08-05 14:04:40 +0800

    logx

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express';
+// 连接数据库
 import db from './mongodb/db.js';
+// config-lite 的作用推测是导入配置文件，其默认文件DIR是config
+// https://github.com/nswbmw/config-lite
+// 官方说明config_dir: config directory name, default: config.
 import config from 'config-lite';
+// router形式为function(app){app.use('/v1', v1);...}的回调函数，配置接口路由，各个controller配置请深入routes路径下查看
+// 导入路由配置
 import router from './routes/index.js';
 import cookieParser from 'cookie-parser'
 import session from 'express-session';
+// connect-mongo是用来将connect的session持久化到mongodb中的
+// const session = require('express-session');const MongoStore = require('connect-mongo')(session);app.use(session({ secret: 'foo',store: new MongoStore(options)}));
 import connectMongo from 'connect-mongo';
 import winston from 'winston';
 import expressWinston from 'express-winston';
