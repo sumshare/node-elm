30f8a9ec402f06c057280bf612615e0522c72055
sumshare <sumshare@126.com>
2017-08-22 14:43:39 +0800

    bak

--- app.js
+++ app.js
@@ -15,10 +15,16 @@ import session from 'express-session';
 // connect-mongo是用来将connect的session持久化到mongodb中的
 // const session = require('express-session');const MongoStore = require('connect-mongo')(session);app.use(session({ secret: 'foo',store: new MongoStore(options)}));
 import connectMongo from 'connect-mongo';
+// winston日志框架
 import winston from 'winston';
 import expressWinston from 'express-winston';
 import path from 'path';
+// Middleware to proxy requests through a specified index page, useful for Single Page Applications that utilise the HTML5 History API.
+// 用于hissoty路由模式下得重定向
+// https://github.com/bripkens/connect-history-api-fallback
+// 仔细看一下，hash模式和history模式对刷新页面的处理方式
 import history from 'connect-history-api-fallback';
+// ./middlewares/statistic.js export的是一个实例化对象
 import Statistic from './middlewares/statistic'
 
 const app = express();
@@ -27,7 +33,7 @@ app.all('*', (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  	res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+  res.header("Access-Control-Allow-Credentials", true); //可以带cookies
 	res.header("X-Powered-By", '3.2.1')
 	if (req.method == 'OPTIONS') {
 	  	res.send(200);
@@ -37,6 +43,7 @@ app.all('*', (req, res, next) => {
 });
 
 app.use(Statistic.apiRecord)
+// session 信息封装
 const MongoStore = connectMongo(session);
 app.use(cookieParser());
 app.use(session({
@@ -49,7 +56,7 @@ app.use(session({
 	  	url: config.url
 	})
 }))
-
+// 生成日志文件配置
 app.use(expressWinston.logger({
     transports: [
         new (winston.transports.Console)({
@@ -77,5 +84,7 @@ app.use(expressWinston.errorLogger({
 }));
 
 app.use(history());
+// 启用项目
 app.use(express.static('./public'));
+// 端口8001
 app.listen(config.port);
